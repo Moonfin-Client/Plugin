cbcca8b29754e0dadd07ebb181fb4f5b3f14137d
mattsigal <mattsigal@users.noreply.github.com>
2026-08-04 07:42:27 -0700

    Add DTO profile properties for new playback time and row settings
    Add server settings profile DTO properties matching Moonfin-Core preferences:
    - musicPlaybackTimeDisplay
    - playbackTimeAboveLeft, playbackTimeAboveCenter, playbackTimeAboveRight
    - playbackTimeBelowLeft, playbackTimeBelowCenter, playbackTimeBelowRight
    - collectionsRowShowEpisodes, mergeRecentRowsByType, playlistsRowShowEpisodes

--- Emby/Emby.Plugins.Moonfin/Models/MoonfinSettingsProfile.cs
+++ Emby/Emby.Plugins.Moonfin/Models/MoonfinSettingsProfile.cs
@@ -273,6 +273,16 @@ namespace Emby.Plugins.Moonfin.Models
         [JsonPropertyName("showSeerrButton")] public bool? ShowSeerrButton { get; set; }
         [JsonPropertyName("diagnosticLoggingEnabled")] public bool? DiagnosticLoggingEnabled { get; set; }
         [JsonPropertyName("updateNotificationsEnabled")] public bool? UpdateNotificationsEnabled { get; set; }
+        [JsonPropertyName("musicPlaybackTimeDisplay")] public string? MusicPlaybackTimeDisplay { get; set; }
+        [JsonPropertyName("playbackTimeAboveLeft")] public string? PlaybackTimeAboveLeft { get; set; }
+        [JsonPropertyName("playbackTimeAboveCenter")] public string? PlaybackTimeAboveCenter { get; set; }
+        [JsonPropertyName("playbackTimeAboveRight")] public string? PlaybackTimeAboveRight { get; set; }
+        [JsonPropertyName("playbackTimeBelowLeft")] public string? PlaybackTimeBelowLeft { get; set; }
+        [JsonPropertyName("playbackTimeBelowCenter")] public string? PlaybackTimeBelowCenter { get; set; }
+        [JsonPropertyName("playbackTimeBelowRight")] public string? PlaybackTimeBelowRight { get; set; }
+        [JsonPropertyName("collectionsRowShowEpisodes")] public bool? CollectionsRowShowEpisodes { get; set; }
+        [JsonPropertyName("mergeRecentRowsByType")] public bool? MergeRecentRowsByType { get; set; }
+        [JsonPropertyName("playlistsRowShowEpisodes")] public bool? PlaylistsRowShowEpisodes { get; set; }
     }
 
     /// <summary>
