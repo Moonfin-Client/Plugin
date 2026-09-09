eec3530e76f11cf54c95b762db12c0e8766a3089
mattsigal <mattsigal@users.noreply.github.com>
2026-09-08 21:11:08 -0700

    Add ModernCardsOnMyMediaRow setting support to Moonbase
    - Add ModernCardsOnMyMediaRow to MoonfinSettingsProfile in Jellyfin and Emby
    - Add Modern cards on My Media row default user setting and description in configPage.html
    - Bind ModernCardsOnMyMediaRow in moonfin.js for settings save and load

--- Emby/Emby.Plugins.Moonfin/Pages/moonfin.js
+++ Emby/Emby.Plugins.Moonfin/Pages/moonfin.js
@@ -1896,6 +1896,7 @@ define(['baseView', 'loading', 'emby-input', 'emby-button', 'emby-checkbox', 'em
             setNullableBoolSelect(view, '#DefaultCinemaModeEpisodesEnabled', defaults.cinemaModeEpisodesEnabled);
 
             setSelectValue(view, '#DefaultHomeRowsStyle', defaults.homeRowsStyle, 'Configured style');
+            setNullableBoolSelect(view, '#DefaultModernCardsOnMyMediaRow', defaults.modernCardsOnMyMediaRow);
             setNullableBoolSelect(view, '#DefaultFullScreenRows', defaults.fullScreenRows);
             setNullableBoolSelect(view, '#DefaultHomeRowInfoOverlay', defaults.homeRowInfoOverlay);
             bindNullableRangeInput(view, '#DefaultClassicHomeRowsPadding', 'px');
@@ -2167,6 +2168,7 @@ define(['baseView', 'loading', 'emby-input', 'emby-button', 'emby-checkbox', 'em
             }
 
             d.homeRowsStyle = view.querySelector('#DefaultHomeRowsStyle').value || null;
+            d.modernCardsOnMyMediaRow = getNullableBoolSelect(view, '#DefaultModernCardsOnMyMediaRow');
             d.fullScreenRows = getNullableBoolSelect(view, '#DefaultFullScreenRows');
             d.homeRowInfoOverlay = getNullableBoolSelect(view, '#DefaultHomeRowInfoOverlay');
             d.classicHomeRowsPadding = getNullableRangeInput(view, '#DefaultClassicHomeRowsPadding');
